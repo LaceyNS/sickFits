f9c03e27024ecc7480963080c853dec3d15ff985
Lacey Savage <lacey.savage@gmail.com>
2021-09-08 20:36:36 -0400

    add font and some basic stylig to GlobalStyles

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7711b274e0741ddf5d8a";
+/******/ 	var hotCurrentHash = "b9544ebfcd5317c06dd8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
