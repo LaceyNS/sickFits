57e5eb23b25609da5587a096afe0f7e2edb652f0
Lacey Savage <lacey.savage@gmail.com>
2021-09-21 17:42:59 -0400

    fix db

--- backend/seed-data/keystone.ts
+++ backend/seed-data/keystone.ts
@@ -1,8 +1,33 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
 
-const databaseURL = process.env/ || 'mongodb://localhost/keystone-sick-fits-tutorial';
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
 const sessionConfig = {
-    maxAge: 60 * 60 * 24 * 30, //How long should they stay signed in. seconds, minutes, hours, days
-    secret: process.env.COOKIE_SECRET,
+  maxAge: 60 * 60 * 24 * 30, // How long should they stay signed in. seconds, minutes, hours, days
+  secret: process.env.COOKIE_SECRET,
 };
+
+export default config({
+  // @ts-ignore
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TO DO: Add data seeding here
+  },
+  lists: createSchema({
+    // Scheme items go in here
+  }),
+  ui: {
+    // TO DO: change this for roles
+    isAccessAllowed: () => true,
+    // TO DO: add session values here
+  },
+});
