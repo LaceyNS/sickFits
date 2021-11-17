caee91d070ac4e86ec28f01c18e8f2fbdc84f2b2
Lacey Savage <lacey.savage@gmail.com>
2021-11-17 17:14:06 -0500

    add if statement for files to adjust the files position in the array

--- frontend/lib/useForm.js
+++ frontend/lib/useForm.js
@@ -10,12 +10,18 @@ export default function useForm(initial = {}) {
     if (type === 'number') {
       value = parseInt(value);
     }
+    // to ensure that if its a file and needs to be an array of files, the first item of the array is the file
+    if (type === 'file') {
+      value[0] = e.target.files;
+    }
     setInputs({
       // copy the existing state
       ...inputs,
       [name]: value,
     });
   }
+
+  
 
   // return the things we want to surface from this custom hook
   return {
