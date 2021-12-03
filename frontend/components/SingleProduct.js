358386404e2acb36a72056ca2d24fe350462f4b5
Lacey Savage <lacey.savage@gmail.com>
2021-12-03 18:48:37 -0500

    add description, image, and altText

--- frontend/components/SingleProduct.js
+++ frontend/components/SingleProduct.js
@@ -1,23 +1,43 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import DisplayError from './ErrorMessage';
 
 const SINGLE_ITEM_QUERY = gql`
-  query {
-    Product(where: { id: "61a915a860f2650cfbbfc8ce" }) {
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
       name
       price
       description
+      id
+      photo {
+        altText
+        image {
+          publicUrlTransformed
+        }
+      }
     }
   }
 `;
 
 export default function SingleProduct({ id }) {
-  const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY);
+  const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY, {
+    variables: {
+      id,
+    },
+  });
   if (loading) return <p>Loading...</p>;
   if (error) return <DisplayError error={error} />;
+  const { Product } = data;
   return (
     <div>
-      <h2>{data.Product.name}</h2>
+      <div className="details">
+        <img
+          src={Product.photo.image.publicUrlTransformed}
+          alt={Product.photo.altText}
+        />
+        <h2>{Product.name}</h2>
+        <p>{Product.description}</p>
+      </div>
     </div>
   );
 }
