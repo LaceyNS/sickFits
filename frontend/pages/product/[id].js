5f6c97f0c4f65f61eb75443a65dc36dfd0dee711
Lacey Savage <lacey.savage@gmail.com>
2021-12-03 14:53:10 -0500

    add gql query

--- frontend/pages/product/[id].js
+++ frontend/pages/product/[id].js
@@ -1,3 +1,18 @@
-export default function SingleProduct() {
-  return <p>Hey Im a single product</p>;
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const SINGLE_ITEM_QUERY = gql`
+  query {
+    Product(where: { id: "61a915a860f2650cfbbfc8ce" }) {
+      name
+      price
+      description
+    }
+  }
+`;
+
+export default function SingleProduct({ query }) {
+  const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY);
+  console.log({ data, loading, error });
+  return <p>Hey Im a single product {query.id}</p>;
 }
