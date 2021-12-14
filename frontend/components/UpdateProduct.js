2793e2b833242bb972478b44824bdea557305d92
Lacey Savage <lacey.savage@gmail.com>
2021-12-13 19:23:12 -0500

    add query to get product on the update page

--- frontend/components/UpdateProduct.js
+++ frontend/components/UpdateProduct.js
@@ -1,3 +1,24 @@
-export default function UpdateProduct() {
-  return <p>Update!</p>;
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+
+const SINGLE_PRODUCT_QUERY = gql`
+  query SINGLE_PRODUCT_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+export default function UpdateProduct({ id }) {
+  // 1. We need to get the existing product
+  const { data, error, loading } = useQuery(SINGLE_PRODUCT_QUERY, {
+    variables: { id },
+  });
+  console.log(data);
+  // 2. We need to get the mutation to update the product
+  // 3. We need the form to handle the updates
+  return <p>Update {id}!</p>;
 }
