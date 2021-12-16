dc54ffdd51834b309f5ab88483e6ffd0d428d870
Lacey Savage <lacey.savage@gmail.com>
2021-12-16 16:32:15 -0500

    create state for the form inputs

--- frontend/components/UpdateProduct.js
+++ frontend/components/UpdateProduct.js
@@ -1,5 +1,8 @@
 import gql from 'graphql-tag';
 import { useMutation, useQuery } from '@apollo/client';
+import DisplayError from './ErrorMessage';
+import useForm from '../lib/useForm';
+import Form from './styles/Form.js';
 
 const SINGLE_PRODUCT_QUERY = gql`
   query SINGLE_PRODUCT_QUERY($id: ID!) {
@@ -47,6 +50,72 @@ export default function UpdateProduct({ id }) {
       //   TO DO: Pass in updates to product here
     },
   });
+  //   2.5. Create some state for the form inputs
+  const { inputs, handleChange, resetForm, clearForm } = useForm(data?.Product);
+  console.log(inputs);
+  if (loading) return <p>Loading...</p>;
   // 3. We need the form to handle the updates
+  return (
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        // Submit the input fields to the backend
+        // TO DO: Handle Submit
+        // const res = await createProduct();
+        // clearForm();
+        // // Go to that product's page
+        // Router.push({
+        //   pathname: `/product/${res.data.createProduct.id}`,
+        // });
+      }}
+    >
+      <DisplayError error={error || updateError} />
+      <fieldset disabled={updateLoading} aria-busy={updateLoading}>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="Price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+
+        {/* if we wanted to have reset and clear buttons */}
+        {/* <button type="button" onClick={clearForm}>
+        Clear Form
+      </button>
+      <button type="button" onClick={resetForm}>
+        Reset Form
+      </button> */}
+
+        <button type="submit">Update Product</button>
+      </fieldset>
+    </Form>
+  );
+
   return <p>Update {id}!</p>;
 }
