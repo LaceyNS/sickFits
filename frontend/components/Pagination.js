eb8df57d1e678fc9720e11b868ecaf13ba3df9b0
Lacey Savage <lacey.savage@gmail.com>
2022-01-27 17:53:02 -0500

    add page number and item total

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import PaginationStyles from './styles/PaginationStyles';
 import DisplayError from './ErrorMessage';
+import { perPage } from '../config';
 
 const PAGINATION_QUERY = gql`
   query PAGINATION_QUERY {
@@ -16,15 +17,17 @@ const PAGINATION_QUERY = gql`
 export default function Pagination({ page }) {
   const { error, loading, data } = useQuery(PAGINATION_QUERY);
   if (loading) return 'Loading...';
-  if (error) return DisplayError;
+  if (error) return <DisplayError error={error} />;
+  const { count } = data._allProductsMeta;
+  const pageCount = Math.ceil(count / perPage);
   return (
     <PaginationStyles>
       <Head>
         <title>Sick Fits - Page {page} of ___</title>
       </Head>
       <Link href="/">← Prev</Link>
-      <p>Page __ of ___</p>
-      <p>___ Items Total</p>
+      <p>Page __ of {pageCount}</p>
+      <p>{count} Items Total</p>
       <Link href="/">Next →</Link>
     </PaginationStyles>
   );
