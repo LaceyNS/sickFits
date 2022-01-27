b93ffcbee10c8fe861c71f9d40027ec207b6c6ec
Lacey Savage <lacey.savage@gmail.com>
2022-01-27 18:08:58 -0500

    Change config number per page, add Prev page link and format

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -23,12 +23,18 @@ export default function Pagination({ page }) {
   return (
     <PaginationStyles>
       <Head>
-        <title>Sick Fits - Page {page} of ___</title>
+        <title>
+          Sick Fits - Page {page} of {pageCount}
+        </title>
       </Head>
-      <Link href="/">← Prev</Link>
-      <p>Page __ of {pageCount}</p>
+      <Link href={`/products/${page - 1}`}>
+        <a aria-disabled={page <= 1}>← Prev</a>
+      </Link>
+      <p>
+        Page {page} of {pageCount}
+      </p>
       <p>{count} Items Total</p>
-      <Link href="/">Next →</Link>
+      <Link href={`/products/${page + 1}`}>Next →</Link>
     </PaginationStyles>
   );
 }
