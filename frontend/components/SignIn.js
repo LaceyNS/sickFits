273e25640c4bcf99f3c529ba249d8ed47a3ca195
Lacey Savage <lacey.savage@gmail.com>
2022-02-17 18:44:06 -0500

    link signin component and page, create form for sign in

---
+++ frontend/components/SignIn.js
@@ -0,0 +1,38 @@
+import Form from './styles/Form';
+import useForm from '../lib/useForm';
+
+export default function SignIn() {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+    password: '',
+  });
+  return (
+    <Form method="POST">
+      <fieldset>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email Address"
+            autoComplete="email"
+            //value
+            //onChange
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            autoComplete="password"
+            //value
+            //onChange
+          />
+        </label>
+        <button type="submit">Sign In!</button>
+      </fieldset>
+    </Form>
+  );
+}
