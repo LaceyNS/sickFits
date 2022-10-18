c1bd458caaed7c9de0e34e3474ba5f58624af04a
Lacey Savage <lacey.savage@gmail.com>
2022-10-18 16:06:18 -0400

    create cart.js and add to Header.js and User.js

---
+++ frontend/components/Cart.js
@@ -0,0 +1,25 @@
+import CartStyles from './styles/CartStyles';
+import Supreme from './styles/Supreme';
+import { useUser } from './User';
+
+function CartItem({ cartItem }) {
+  return <li>{cartItem.id}</li>;
+}
+
+export default function Cart() {
+  const me = useUser();
+  if (!me) return null;
+  console.log(me);
+  return (
+    <CartStyles open>
+      <header>
+        <Supreme>{me.name}'s Cart</Supreme>
+      </header>
+      <ul>
+        {me.cart.map((cartItem) => (
+          <CartItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+      </ul>
+    </CartStyles>
+  );
+}
