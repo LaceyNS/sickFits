178382adf254835cf5e9526355b4c0b1de0a8546
Lacey Savage <lacey.savage@gmail.com>
2022-10-18 16:25:02 -0400

    style cart and add money formatting

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,9 +1,45 @@
+import styled from 'styled-components';
+import formatMoney from '../lib/formatMoney';
 import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import { useUser } from './User';
 
+const CartItemStyles = styled.li`
+  padding: 1rem 0;
+  border-bottom: 1px solid var(--lightGrey);
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  img {
+    margin-right: 1rem;
+  }
+  h3,
+  p {
+    margin: 0;
+  }
+`;
+
 function CartItem({ cartItem }) {
-  return <li>{cartItem.id}</li>;
+  const product = cartItem.product;
+  if (!product) return null;
+  console.log(product);
+  return (
+    <CartItemStyles>
+      <img
+        width="100"
+        src={product.photo.image.publicUrlTransformed}
+        alt={product.name}
+      />
+      <div>
+        <h3>{product.name}</h3>
+        <p>
+          {formatMoney(product.price * cartItem.quantity)}-
+          <em>
+            {cartItem.quantity} &times; {formatMoney(product.price)} each
+          </em>
+        </p>
+      </div>
+    </CartItemStyles>
+  );
 }
 
 export default function Cart() {
