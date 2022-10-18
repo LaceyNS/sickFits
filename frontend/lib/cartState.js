f5edc2d5f86c0c12a5f6283f91116847f1e566fe
Lacey Savage <lacey.savage@gmail.com>
2022-10-18 17:38:25 -0400

    add toggleCart and close open cart buttons and remove some console logs

--- frontend/lib/cartState.js
+++ frontend/lib/cartState.js
@@ -1,14 +1,32 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
 
 const LocalStateContext = createContext();
 const LocalStateProvider = LocalStateContext.Provider;
 
 function CartStateProvider({ children }) {
   //This is our own custom provider! We will store data (state) and functionality (updateres) in here and anyone can access it via the consumer!
-  const cartOpen = true;
+
+  //Closed cart by default
+  const [cartOpen, setCartOpen] = useState(false);
+
+  function toggleCart() {
+    setCartOpen(!cartOpen);
+  }
+
+  function closeCart() {
+    setCartOpen(false);
+  }
+
+  function openCart() {
+    setCartOpen(true);
+  }
 
   return (
-    <LocalStateProvider value={{ cartOpen }}>{children}</LocalStateProvider>
+    <LocalStateProvider
+      value={{ cartOpen, setCartOpen, toggleCart, closeCart, openCart }}
+    >
+      {children}
+    </LocalStateProvider>
   );
 }
 
