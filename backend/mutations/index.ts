702d293000b9cf885f3c8ae7d862472b8e3eaa6a
Lacey Savage <lacey.savage@gmail.com>
2022-10-27 14:54:44 -0400

    add mutations/index and add typeDefs

---
+++ backend/mutations/index.ts
@@ -0,0 +1,11 @@
+import { graphQLSchemaExtension } from "@keystone-next/keystone/schema";
+
+// make a fake graphql tagged template literal
+const graphql = String.raw;
+export const extendGraphqlSchema = graphQLSchemaExtension({
+    typeDefs: graphql`
+    type Mutation {
+        addToCart(productID: ID): CartItem
+    }`
+    // resolvers
+})
