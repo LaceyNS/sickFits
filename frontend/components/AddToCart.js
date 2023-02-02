2f187d6e5f1061c7d0057a7d3a94a78403077f6c
Lacey Savage <lacey.savage@gmail.com>
2023-02-02 13:25:22 -0500

    create AddToCart button

---
+++ frontend/components/AddToCart.js
@@ -0,0 +1,10 @@
+const ADD_TO_CART_MUTATION = gql`
+  mutation ADD_TO_CART_MUTATION($id: ID!) {
+    addToCart(productId: $id) {
+      id
+    }
+  }
+`;
+export default function AddToCart({ id }) {
+  return <button type="button">Add To Cart 🛒</button>;
+}
