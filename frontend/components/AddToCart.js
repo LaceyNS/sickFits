01fd0e51662e9455e0cc3eb0a3722382dca13bfd
Lacey Savage <lacey.savage@gmail.com>
2023-02-02 13:39:19 -0500

    add mutation and loading state to AddToCart button

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -1,3 +1,7 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
+
 const ADD_TO_CART_MUTATION = gql`
   mutation ADD_TO_CART_MUTATION($id: ID!) {
     addToCart(productId: $id) {
@@ -6,5 +10,13 @@ const ADD_TO_CART_MUTATION = gql`
   }
 `;
 export default function AddToCart({ id }) {
-  return <button type="button">Add To Cart 🛒</button>;
+  const [addToCart, { loading }] = useMutation(ADD_TO_CART_MUTATION, {
+    variables: { id },
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+  return (
+    <button disabled={loading} type="button" onClick={addToCart}>
+      Add{loading && 'ing'} To Cart 🛒
+    </button>
+  );
 }
