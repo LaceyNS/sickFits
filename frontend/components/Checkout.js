960bab731cc03772a62bc3caee971c2c967dfa4e
Lacey Savage <lacey.savage@gmail.com>
2023-02-13 15:10:56 -0500

    add checkout with stripe

--- frontend/components/Checkout.js
+++ frontend/components/Checkout.js
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import { loadStripe } from '@stripe/stripe-js';
+import { CardElement, Elements } from '@stripe/react-stripe-js';
+import SickButton from './styles/SickButton';
 
 const CheckoutFormStyles = styled.form`
   box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
@@ -9,11 +12,21 @@ const CheckoutFormStyles = styled.form`
   grid-gap: 1rem;
 `;
 
+const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
+
 function Checkout() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('We gotta do some work..');
+  }
+
   return (
-    <CheckoutFormStyles>
-      <p>Hey</p>
-    </CheckoutFormStyles>
+    <Elements stripe={stripeLib}>
+      <CheckoutFormStyles onSubmit={handleSubmit}>
+        <CardElement />
+        <SickButton>Check Out Now</SickButton>
+      </CheckoutFormStyles>
+    </Elements>
   );
 }
 
