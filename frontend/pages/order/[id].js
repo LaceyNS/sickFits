33e88a774c7c7c792c9eefb0893acb7e5f7af2cb
Lacey Savage <lacey.savage@gmail.com>
2023-03-03 15:34:33 -0500

    create single order page and single order query

---
+++ frontend/pages/order/[id].js
@@ -0,0 +1,38 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import ErrorMessage from '../../components/ErrorMessage';
+
+const SINGLE_ORDER_QUERY = gql`
+  query SINGLE_ORDER_QUERY($id: ID!) {
+    order: Order(where: { id: $id }) {
+      id
+      charge
+      total
+      user {
+        id
+      }
+      items {
+        id
+        name
+        description
+        price
+        quantity
+        photo {
+          image {
+            publicUrlTransformed
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default function SingleOrderPage({ query }) {
+  const { data, error, loading } = useQuery(SINGLE_ORDER_QUERY, {
+    variables: { id: query.id },
+  });
+  if (loading) return <p>Loading...</p>;
+  if (error) return <ErrorMessage error={error} />;
+  const { order } = data;
+  return <div>Hey {order.id}</div>;
+}
