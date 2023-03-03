8941cb12963fdecf04839c35531add89a150e877
Lacey Savage <lacey.savage@gmail.com>
2023-03-03 15:50:57 -0500

    create id order page and populate info from order into it

--- frontend/pages/order/[id].js
+++ frontend/pages/order/[id].js
@@ -1,6 +1,9 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import ErrorMessage from '../../components/ErrorMessage';
+import OrderStyles from '../../components/styles/OrderStyles';
+import Head from 'next/head';
+import formatMoney from '../../lib/formatMoney';
 
 const SINGLE_ORDER_QUERY = gql`
   query SINGLE_ORDER_QUERY($id: ID!) {
@@ -34,5 +37,35 @@ export default function SingleOrderPage({ query }) {
   if (loading) return <p>Loading...</p>;
   if (error) return <ErrorMessage error={error} />;
   const { order } = data;
-  return <div>Hey {order.id}</div>;
+  return (
+    <OrderStyles>
+      <Head>
+        <title>Sick Fits - {order.id}</title>
+      </Head>
+      <p>
+        <span>Order Id:</span>
+        <span>{order.id}</span>
+        <span>Charge:</span>
+        <span>{order.charge}</span>
+        <span>Order Total:</span>
+        <span>{formatMoney(order.total)}</span>
+        <span>Item Count:</span>
+        <span>{order.items.length}</span>
+      </p>
+      <div className="items">
+        {order.items.map((item) => (
+          <div className="order-item" key={item.id}>
+            <img src={item.photo.image.publicUrlTransformed} alt={item.title} />
+            <div className="item-details">
+              <h2>{item.name}</h2>
+              <p>Qty: {item.quantity}</p>
+              <p>Each: {formatMoney(item.price)}</p>
+              <p>Sub Total: {formatMoney(item.price * item.quantity)}</p>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </OrderStyles>
+  );
 }
