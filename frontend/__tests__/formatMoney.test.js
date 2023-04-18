38731a249157189aa14682c178ab70157dbe9394
Lacey Savage <lacey.savage@gmail.com>
2023-04-18 13:19:21 -0400

    add test for combined whole and fractional amounts

--- frontend/__tests__/formatMoney.test.js
+++ frontend/__tests__/formatMoney.test.js
@@ -6,12 +6,19 @@ describe('format Money function', () => {
     expect(formatMoney(10)).toEqual('CA$0.10');
     expect(formatMoney(9)).toEqual('CA$0.09');
     expect(formatMoney(40)).toEqual('CA$0.40');
-    expect(formatMoney(140)).toEqual('CA$1.40');
   });
 
   it('leaves off cents when its whole dollars', () => {
     expect(formatMoney(5000)).toEqual('CA$50');
     expect(formatMoney(100)).toEqual('CA$1');
     expect(formatMoney(50000000)).toEqual('CA$500,000');
+  });
+
+  it('works with whole and fractional dollars', () => {
+    expect(formatMoney(140)).toEqual('CA$1.40');
+    expect(formatMoney(5012)).toEqual('CA$50.12');
+    expect(formatMoney(110)).toEqual('CA$1.10');
+    expect(formatMoney(109)).toEqual('CA$1.09');
+    expect(formatMoney(345345999)).toEqual('CA$3,453,459.99');
   });
 });
